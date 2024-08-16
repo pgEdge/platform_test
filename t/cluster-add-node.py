ea3bfba64705f9118b5746ffaf57333c0d8bb66b
susan-pgedge <ec2-user@ip-172-31-11-85.ec2.internal>
2024-08-16 14:28:34 +0000

    Updates to cluster-add-node.py to add check for line in bashrc and to clean up the .json files after running

--- t/cluster-add-node.py
+++ t/cluster-add-node.py
@@ -21,20 +21,28 @@ spockpath=os.getenv("EDGE_SPOCK_PATH")
 dbname=os.getenv("EDGE_DB","lcdb")
 
 
-import os
-
-# Get the current working directory
+## Get the current working directory
 current_directory = os.getcwd()
-
 print(f"Current Working Directory: {current_directory}")
-
 
 ## Meet prerequisite: add export PGBACKREST_REPO1_CIPHER_PASS=YourCipherPassHere to ~/.bashrc
 
-command = 'export PGBACKREST_REPO1_CIPHER_PASS=YourCipherPassHere'
+# Check the ~/.bashrc file for the pgBackRest command and add it if it's missing:
+export_pgbackrest = 'export PGBACKREST_REPO1_CIPHER_PASS=YourCipherPassHere'
 bashrc_path = os.path.expanduser('~/.bashrc')
-with open(bashrc_path, 'a') as file:
-    file.write(command)
+
+# Read the .bashrc file
+with open(bashrc_path, 'r') as file:
+    lines = file.readlines()
+
+# Check if the line is already in the file and add it if it is not
+if export_pgbackrest + '\n' not in lines:
+    # If not, append it to the file
+    with open(bashrc_path, 'a') as file:
+        file.write(export_pgbackrest + '\n')
+    print(f"pgBackRest export statement added to {bashrc_path}.")
+else:
+    print("The pgBackRest export statement is already in the .bashrc file.")
 
 
 ## Meet prerequisite: Delete any pgBackRest artifacts with: rm -rf /etc/pgbackrest
@@ -49,38 +57,38 @@ else:
     print(f'Directory {directory} does not exist.')
 
 ## Create the json file for n4:
+
 data = {'json_version': 1.0, 'node_groups': [{'ssh': {'os_user': 'ec2-user', 'private_key': ''}, 'name': 'n4', 'is_active': 'on', 'public_ip': '127.0.0.1', 'private_ip': '127.0.0.1',
 'port': '6435', 'path': '/home/ec2-user/work/platform_test/nc/pgedge/cluster/demo/n4'}]}
 
 file_name = 'n4.json'
 
-# Write the data to the JSON file
+## Write the node description to the JSON file
 with open(file_name, 'w') as json_file:
     json.dump(data, json_file, indent=4)
 
-# Move the n4.json file to home_dir:
+## Move the n4.json file to home_dir:
 
 source = (f"n4.json")
 target = (f"{home_dir}/n4.json")
-print(f"home_dir = {home_dir}\n")
+#print(f"home_dir = {home_dir}\n")
 print(f"We need to copy that file to: {home_dir}")
-shutil.copyfile(source, target)
+shutil.move(source, target)
 print("*"*100)
 
 
-# Then invoke cluster add-node:
+## Then invoke cluster add-node:
 
 data_source = "n1"
 data_target = "n4"
 
 command = (f"cluster add-node {cluster_name} {data_source} {data_target}")
 init=util_test.run_nc_cmd("This command should initialize a cluster based on the json file", command, f"{home_dir}")
-print(f"init = {init.stdout}\n")
+#print(f"init = {init.stdout}\n")
 print("*"*100)
 
 
-# Needle and Haystack
-# Confirm the command worked by looking for:
+## Needle and Haystack - Confirm the command worked by looking for:
 
 check=util_test.contains((init.stdout),"sub_n4n2")
 print("*"*100)
