37d113704584e328088a0b11ff82ad7373d97ca4
susan-pgedge <ec2-user@ip-172-31-11-85.ec2.internal>
2024-09-03 17:29:04 +0000

    Updates to spock_4.0 schedule file/exception_table_case4.py, and t/util_test.py (added function to enable autoddl)

--- t/spock_exception_table_case4.py
+++ t/spock_exception_table_case4.py
@@ -1,4 +1,4 @@
-import sys, os, util_test,subprocess
+import sys, os, util_test, subprocess, time
 
 ## Print Script
 print(f"Starting - {os.path.basename(__file__)}")
@@ -18,139 +18,109 @@ repuser=os.getenv("EDGE_REPUSER","pgedge")
 repset=os.getenv("EDGE_REPSET","demo-repset")
 spockpath=os.getenv("EDGE_SPOCK_PATH")
 dbname=os.getenv("EDGE_DB","lcdb")
+seconds=int(os.getenv("EDGE_SLEEP"))
 
-port2=port1+1
+port2 = port1+1
 print(port2)
+nc_dir=os.getenv("NC_DIR","nc")
+home_dir = os.getenv("EDGE_HOME_DIR") 
 
-## pgbench-install on n1
-## CONFIRM that if a database name and repset name are provided, pgbench is installed as expected and the transactions are added to the repset
-cmd_node = f"app pgbench-install {dbname} -r default"
-res=util_test.run_cmd("running pgbench-install including repsetname", cmd_node, f"{cluster_dir}/n1")
-print(f"The installation on n1 returns: {res}")
-print("*"*100) 
-
-## pgbench-install in n2
-## CONFIRM that if a database name and repset name are provided, pgbench is installed as expected and the transactions are added to the repset
-cmd_node = f"app pgbench-install {dbname} -r default"
-res=util_test.run_cmd("running pgbench-install including repsetname", cmd_node, f"{cluster_dir}/n2")
-print(f"The installation on n2 returns: {res}")
+## Check the information from cluster list-nodes.
+command = (f"cluster list-nodes demo")
+res=util_test.run_nc_cmd("Exercise the list-nodes command", command, f"{home_dir}")
+print(f"Command: {command}")
+print(f"The list-nodes command returns = {res}\n")
 print("*"*100)
 
-## Use needle/haystack to confirm pgbench is installed on n1
-## confirm with SELECT * FROM spock.tables.
-row = util_test.read_psql("SELECT * FROM spock.tables",host,dbname,port1,pw,usr).strip("[]")
-check=util_test.contains((row),"default")
-print(f"The n1 check returns: {row}")
+## Setup on n1 
+## Create a table with three columns:
+command1 = "CREATE TABLE case4 (bid integer PRIMARY KEY, bbalance integer, filler character(88))"
+row1 = util_test.write_psql(command1,host,dbname,port1,pw,usr)
+#print(f"The create table statement on n1 returns: {row1}")
+
+## Add a row:
+command2 = "INSERT INTO case4 VALUES (1, 11111, 'filler')"
+print(f"{command2}")
+row2 = util_test.write_psql(command2,host,dbname,port1,pw,usr)
+#print(f"The insert statement on n1 returns: {row2}")
+
+## Add it to the default repset:
+command3 = f"spock repset-add-table default case4 {dbname}"
+res3=util_test.run_cmd("Adding our table to the default repset", command3, f"{cluster_dir}/n1")
+print(f"The repset-add-table command on n1 returns: {res3}")
 print("*"*100)
 
-## Use needle/haystack to confirm pgbench is installed on n2.
-## confirm with SELECT * FROM pgbench_branches on n2.
-row = util_test.read_psql("SELECT * FROM pgbench_branches",host,dbname,port2,pw,usr)
-#check=util_test.contains((row),"default")
-print(f"The n2 check returns: {row}")
+## Setup on n2
+## Create a table with the same name as the table on n1, but with two columns:
+command4 = "CREATE TABLE case4 (bid integer PRIMARY KEY, bbalance integer)"
+row4 = util_test.write_psql(command4,host,dbname,port2,pw,usr)
+#print(f"The create table statement on n2 returns: {row4}")
+
+## Add a row:
+command5 = "INSERT INTO case4 VALUES (1, 11111)"
+row5 = util_test.write_psql(command5,host,dbname,port2,pw,usr)
+#print(f"The insert statement on n2 returns: {row5}")
+
+## Add it to the default repset:
+command6 = f"spock repset-add-table default case4 {dbname}"
+res6=util_test.run_cmd("Adding our table to the default repset", command6, f"{cluster_dir}/n2")
+print(f"The repset-add-table command on n2 returns: {res6}")
 print("*"*100)
 
-## In this example, n1 has one row in our sample table (foo) with columns a, b, and c.
-## Node n2 has one row in our sample table (foo) with columns a and b.
-
-## Remove a column from the pgbench_branches table on n2:
-
-row = util_test.write_psql("ALTER TABLE pgbench_branches DROP COLUMN filler CASCADE",host,dbname,port2,pw,usr)
-print(f"We just removed the filler column from the pgbench_branches table on n2: {row}")
+## Confirm with SELECT * FROM spock.tables.
+row7 = util_test.read_psql("SELECT relname FROM spock.tables;",host,dbname,port1,pw,usr)
+print(f"The n1 select * from spock.tables returns: {row7}")
 print("*"*100)
 
-
-print("We're going to start here tomorrow")
-
-
-
-## Add one row that should be replicated from n1 to n2:
-
-row = util_test.write_psql("INSERT INTO pgbench_branches VALUES(11, 11000, null)",host,dbname,port1,pw,usr)
-print(f"We inserted bid 11 on n1: {row}")
-print("*"*100)
-
-## Look for our rows on n1 and n2:
-
-row1 = util_test.read_psql("SELECT * FROM pgbench_branches",host,dbname,port1,pw,usr)
-print(f"Node n1 should contain bid 1/11: {row1}")
-
-row2 = util_test.read_psql("SELECT * FROM pgbench_branches",host,dbname,port2,pw,usr)
-print(f"Node n2 should contain bid 1/11: {row2}")
-
-print("*"*100)
-
-## Create an anonymous block that puts the cluster in repair mode and does an insert statement that will
-## add a row to n2 that will not be replicated to n1:
-
-anon_block = """
-DO $$
-BEGIN
-    PERFORM spock.repair_mode('True');
-    INSERT INTO pgbench_branches VALUES (22, 22000, null);
-END $$;
-"""
-
-print(anon_block)
-row = util_test.write_psql(f"{anon_block}",host,dbname,port2,pw,usr)
-print(row)
-
-## Check the rows on n1 and n2:
-
-row1 = util_test.read_psql("SELECT * FROM pgbench_branches",host,dbname,port1,pw,usr)
-print(f"We're in repair mode - n1 now contains 1/11: {row1}")
-
-row2 = util_test.read_psql("SELECT * FROM pgbench_branches",host,dbname,port2,pw,usr)
-print(f"We're in repair mode - n2 now contains 1/11/22: {row2}")
-
-print("*"*100)
-
-## Add a row to n1 that has the same bid as the row we added on n2; we're still in repair mode:
-
-row = util_test.write_psql("INSERT INTO pgbench_branches VALUES(22, 99000, null)",host,dbname,port1,pw,usr)
-print(f"We just tried to insert bid 22 on n1 - this should fail, but it doesn't: {row}")
-print("*"*100)
-
-## Look for our rows on n1 and n2:
-
-row1 = util_test.read_psql("SELECT * FROM pgbench_branches",host,dbname,port1,pw,usr)
-print(f"Node n1 should contain bid 1/11: {row1}")
-
-row2 = util_test.read_psql("SELECT * FROM pgbench_branches",host,dbname,port2,pw,usr)
-print(f"Node n2 should contain bid 1/11/22: {row2}")
-
-
-## Check the results from the statement above, and you can see the duplicate primary key error 
-## is not being caught. Fix this when the patch is in.
-
-
-## Read from the spock.exception_log on n1;
-row = util_test.read_psql("SELECT remote_new_tup FROM spock.exception_log",host,dbname,port1,pw,usr).strip("[]")
-print(f"SELECT remote_new_tup FROM spock.exception_log on n1 returns an empty result set: {row}")
-print("*"*100)
-
-## Read from the spock.exception_log on n2;
-row = util_test.read_psql("SELECT remote_new_tup FROM spock.exception_log",host,dbname,port2,pw,usr).strip("[]")
-print(f"SELECT remote_new_tup FROM spock.exception_log on n2 returns the replication error: {row}")
-print("*"*100)
-
-## Show that the row update hasn't caused a death spiral:
-row = util_test.read_psql("SELECT * FROM pgbench_branches",host,dbname,port1,pw,usr).strip("[]")
-print(f" n1 pgbench branches contains: {row}")
-print("*"*100)
-
-## Show that the row update hasn't caused a death spiral:
-row = util_test.read_psql("SELECT * FROM pgbench_branches",host,dbname,port2,pw,usr).strip("[]")
-print(f" n2 pgbench branches contains: {row}")
-print("*"*100)
-
-## Read from the spock.exception_log on n2 for our needle/haystack step:
-row = util_test.read_psql("SELECT remote_new_tup FROM spock.exception_log",host,dbname,port2,pw,usr)
-print(f"SELECT remote_new_tup FROM spock.exception_log on n2 returns: {row}")
+## Check the values in case4 on n1.
+row7 = util_test.read_psql("SELECT * FROM case4;",host,dbname,port1,pw,usr)
+print(f"The n1 select * from case4 returns: {row7}")
 print("*"*100)
 
 
-if '"value": 22, "attname": "bid", "atttype": "int4"' in str(row):
+## Confirm with SELECT * FROM spock.tables on n2.
+row8 = util_test.read_psql("SELECT relname FROM spock.tables;",host,dbname,port2,pw,usr)
+print(f"The n2 select * from spock.tables returns: {row8}")
+print("*"*100)
+
+## Check the values in case4 on n2.
+row7 = util_test.read_psql("SELECT * FROM case4;",host,dbname,port2,pw,usr)
+print(f"The n2 select * from case4 returns: {row7}")
+print("*"*100)
+
+
+## Enable AutoDDL (uses connection that allows ALTER SYSTEM SET) and reload configuration: 
+n1enable = util_test.enable_autoddl(host, dbname, port1, pw, usr)
+n2enable = util_test.enable_autoddl(host, dbname, port2, pw, usr)
+
+## Check our variable values;
+row = util_test.read_psql("SELECT name, setting FROM pg_settings WHERE NAME LIKE 'spock.%'",host,dbname,port1,pw,usr)
+print(f"SELECT * FROM spock.exception_log on n1 returns: {row}")
+print("*"*100)
+
+## Check our variable values;
+row = util_test.read_psql("SELECT name, setting FROM pg_settings WHERE NAME LIKE 'spock.%'",host,dbname,port2,pw,usr)
+print(f"SELECT * FROM spock.exception_log on n2 returns: {row}")
+print("*"*100)
+
+## Drop the filler column from n1:
+command1 = "ALTER TABLE case4 DROP COLUMN filler"
+row1 = util_test.write_psql(command1,host,dbname,port1,pw,usr)
+#print(f"We just dropped the filler column from n1: {row1}")
+print("*"*100)
+
+## Read from the spock.exception_log;
+row = util_test.read_psql("SELECT remote_new_tup FROM spock.exception_log WHERE table_name = 'queue'",host,dbname,port1,pw,usr)
+print(f"SELECT * FROM spock.exception_log on n1 returns: {row}")
+print("*"*100)
+
+## Read from the spock.exception_log;
+row = util_test.read_psql("SELECT remote_new_tup FROM spock.exception_log WHERE table_name = 'queue';",host,dbname,port2,pw,usr)
+print(f"SELECT * FROM spock.exception_log on n2 returns: {row}")
+print("*"*100)
+
+
+if 'ALTER TABLE case4 DROP COLUMN filler' in str(row):
     
     util_test.EXIT_PASS()
 else:
